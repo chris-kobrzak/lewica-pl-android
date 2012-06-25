ed03a25a10eb0984edbaf50ca5f3e5204c7c4c0c
kobrzak@lewica.pl <kobrzak@lewica.pl@1cfff53c-f52c-0e3a-7529-82c1170abfbd>
2012-06-25 10:57:24 +0000

    Creating branch for version 1.3 of the app.
    The main new feature to be introduced is support for blogs.

---
+++ branches/v.1.3/src/pl/lewica/lewicapl/android/activity/AnnouncementListActivity.java
@@ -0,0 +1,218 @@
+/*
+ Copyright 2011 lewica.pl
+
+ Licensed under the Apache Licence, Version 2.0 (the "Licence");
+ you may not use this file except in compliance with the Licence.
+ You may obtain a copy of the Licence at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the Licence is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the Licence for the specific language governing permissions and
+ limitations under the Licence. 
+*/
+package pl.lewica.lewicapl.android.activity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import pl.lewica.lewicapl.R;
+import pl.lewica.lewicapl.android.database.AnnouncementDAO;
+
+
+/**
+ * @author Krzysztof Kobrzak
+ */
+public class AnnouncementListActivity extends Activity {
+
+	public static final String RELOAD_VIEW	= "pl.lewica.lewicapl.android.activity.announcementslistactivity.RELOAD";
+
+	// When users select a new article, navigate back to the list and start scrolling up and down, the cursor won't know this article should be marked as read.
+	// That results in articles still being marked as unread (titles in red rather than blue).
+	// That's why we need to cache the list of clicked articles.  Please note, it is down to ArcticleActivity to flag articles as read in the database.
+	private static Set<Long> clicked	= new HashSet<Long>();
+
+	private AnnouncementDAO annDAO;
+	private ListAdapter listAdapter;
+	private ListView listView;
+	private AnnouncementsUpdateBroadcastReceiver receiver;
+
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_announcements);	// This comes from this file's name /res/list_announcements.xml
+
+		// Load a list view container from list_announcements.xml
+		listView					= (ListView) findViewById(R.id.list_announcements);
+
+		// Register to receive content update messages
+		IntentFilter filter		= new IntentFilter();
+		filter.addAction(RELOAD_VIEW);
+		receiver					= new AnnouncementsUpdateBroadcastReceiver();	// Instance of an inner class
+		registerReceiver(receiver, filter);
+
+		// Access data
+		annDAO					= new AnnouncementDAO(this);
+		annDAO.open();
+		Cursor cursor			= annDAO.selectLatest();
+
+		// Set list view adapter - this links the view with the data
+		listAdapter				= new AnnouncementsCursorAdapter(this, cursor, false);
+		listView.setAdapter(listAdapter);
+
+		// Clicking on an item should redirect to the details view
+		listView.setOnItemClickListener(new OnItemClickListener() {
+//			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				TextView tv;
+				Context context		= getApplicationContext();
+				Resources res			= context.getResources();
+
+				// Redirect to article details screen
+				Intent intent	= new Intent(context, AnnouncementActivity.class);
+				// Builds a uri in the following format: content://lewicapl/articles/article/[0-9]+
+				Uri uri			= Uri.parse(AnnouncementActivity.BASE_URI + Long.toString(id) );
+				// Passes activity Uri as parameter that can be used to work out ID of requested article.
+				intent.setData(uri);
+				startActivity(intent);
+
+				// Mark current announcement as read by changing its colour...
+				int colour		= res.getColor(R.color.read);
+				tv					= (TextView) view.findViewById(R.id.announcement_item_title);
+				tv.setTextColor(colour);
+				// ... and flagging it in local cache accordingly
+				clicked.add(id);
+
+				return;
+			}
+		});
+	}
+
+
+	public void reloadRows() {
+		CursorAdapter ca	= (CursorAdapter) listAdapter;
+		// Reload rows
+		Cursor newCursor	= annDAO.selectLatest();
+		ca.changeCursor(newCursor);
+	}
+
+
+	// INNER CLASSES
+	private class AnnouncementsUpdateBroadcastReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			reloadRows();
+		}
+	}
+
+
+
+	/**
+	 * Populates the list and makes sure announcements that have already been read are marked accordingly.
+	 * It is static nested class, see http://download.oracle.com/javase/tutorial/java/javaOO/nested.html
+	 * @author Krzysztof Kobrzak
+	 */
+	private static final class AnnouncementsCursorAdapter extends CursorAdapter {
+
+		public LayoutInflater inflater;
+		private static Resources res;
+
+		private int colIndex_ID;
+		private int colIndex_WasRead;
+		private int colIndex_Title;
+		private int colIndex_Where;
+		private int colIndex_When;
+
+
+		AnnouncementsCursorAdapter(Context context, Cursor cursor, boolean autoRequery) {
+			super(context, cursor, autoRequery);
+
+			// Get the layout inflater
+			inflater				= LayoutInflater.from(context);
+
+			res					= context.getResources();
+
+			// Get and cache column indices
+			colIndex_ID					= cursor.getColumnIndex(AnnouncementDAO.FIELD_ID);
+			colIndex_WasRead			= cursor.getColumnIndex(AnnouncementDAO.FIELD_WAS_READ);
+			colIndex_Title				= cursor.getColumnIndex(AnnouncementDAO.FIELD_WHAT);
+			colIndex_Where				= cursor.getColumnIndex(AnnouncementDAO.FIELD_WHERE);
+			colIndex_When				= cursor.getColumnIndex(AnnouncementDAO.FIELD_WHEN);
+		}
+
+		/**
+		 * Responsible for providing views with content and formatting it.
+		 */
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			TextView tv;
+			int colour;
+			// Title
+			tv	= (TextView) view.findViewById(R.id.announcement_item_title);
+			if (cursor.getInt(colIndex_WasRead) == 0 && ! clicked.contains(cursor.getLong(colIndex_ID) ) ) {
+				colour	= res.getColor(R.color.unread);
+			} else {
+				colour	= res.getColor(R.color.read);
+			}
+			tv.setTextColor(colour);
+			tv.setText(cursor.getString(colIndex_Title) );
+			// Where and when?
+			tv	= (TextView) view.findViewById(R.id.announcement_item_details);
+			String where	= cursor.getString(colIndex_Where);
+			String when	= cursor.getString(colIndex_When);
+			tv.setVisibility(View.VISIBLE);
+
+			if (where.length() > 0 && when.length() > 0) {
+				StringBuilder sb	= new StringBuilder();
+				sb.append(where);
+				sb.append(" | ");
+				sb.append(when);
+
+				tv.setText(sb.toString() );
+				return;
+			}
+
+			if (where.length() > 0) {
+				tv.setText(where);
+				return;
+			}
+			if (when.length() > 0) {
+				tv.setText(when);
+				return;
+			}
+			// We are still here - that means both where and when info is empty.
+			tv.setText("");
+			tv.setVisibility(View.GONE);
+		}
+
+
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			return inflater.inflate(R.layout.list_announcements_item, parent, false);
+		}
+	}
+	// End of NewsCursorAdapter
+}
---
+++ branches/v.1.3/src/pl/lewica/lewicapl/android/activity/HistoryListActivity.java
@@ -0,0 +1,172 @@
+/*
+ Copyright 2011 lewica.pl
+
+ Licensed under the Apache Licence, Version 2.0 (the "Licence");
+ you may not use this file except in compliance with the Licence.
+ You may obtain a copy of the Licence at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the Licence is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the Licence for the specific language governing permissions and
+ limitations under the Licence. 
+*/
+package pl.lewica.lewicapl.android.activity;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import pl.lewica.lewicapl.R;
+import pl.lewica.lewicapl.android.ContentUpdateManager;
+import pl.lewica.lewicapl.android.database.HistoryDAO;
+
+
+/**
+ * @author Krzysztof Kobrzak
+ */
+public class HistoryListActivity extends Activity {
+	
+	public static final String RELOAD_VIEW	= "pl.lewica.lewicapl.android.activity.historylistactivity.RELOAD";
+
+	private static Typeface categoryTypeface;
+	private HistoryDAO historyDAO;
+	private ListAdapter listAdapter;
+	private ListView listView;
+	private HistoryUpdateBroadcastReceiver receiver;
+	private 	int month;
+	private 	int day;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list_history);	// This comes from this file's name /res/list_history.xml
+
+		// Load a list view container from list_articles.xml
+		listView					= (ListView) findViewById(R.id.list_history_events);
+
+		// Custom font used by the category headings
+		categoryTypeface	= Typeface.createFromAsset(getAssets(), "Impact.ttf");
+
+		LayoutInflater inflater	= getLayoutInflater();
+		LinearLayout barLayout	= (LinearLayout) inflater.inflate(R.layout.bar_layout, null);
+		listView.addHeaderView(barLayout);
+
+		Calendar cal		= Calendar.getInstance();
+		month				= cal.get(Calendar.MONTH) + 1;
+		day					= cal.get(Calendar.DATE);
+		loadView(month, day);
+
+		// Access data
+		historyDAO			= new HistoryDAO(this);
+		historyDAO.open();
+		Cursor cursor			= historyDAO.select(month, day);
+		startManagingCursor(cursor);
+		
+		// Set list view adapter - this links the view with the data
+		listAdapter				= new SimpleCursorAdapter(
+				this, 
+				R.layout.list_history_item,
+				cursor, 
+				new String[] { HistoryDAO.FIELD_YEAR, HistoryDAO.FIELD_EVENT },
+				new int[] { R.id.history_year, R.id.history_event }
+		) {
+			// Disables the onClick event attached to the rows (and also the default, orange background colour).
+			// In order to get rid of lines dividing the rows, you might want to implement areAllItemsEnabled() as well.
+			@Override
+			public boolean isEnabled(int position) { 
+				return false; 
+			} 
+		}; 
+		listView.setAdapter(listAdapter);
+
+		// Register to receive content update messages
+		IntentFilter filter		= new IntentFilter();
+		filter.addAction(RELOAD_VIEW);
+		receiver					= new HistoryUpdateBroadcastReceiver();	// Instance of an inner class
+		registerReceiver(receiver, filter);
+	}
+
+
+	/**
+	 * When this activity is created, it saves the current day and month.  We compare the current date
+	 * with these values to work out if we should update the content.
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Calendar cal		= Calendar.getInstance();
+		int monthNow	= cal.get(Calendar.MONTH) + 1;
+		int dayNow		= cal.get(Calendar.DATE);
+		
+		if (day == dayNow && month == monthNow) {
+			return;
+		}
+		// Update current date
+		day		= dayNow;
+		month	= monthNow;
+
+		ContentUpdateManager updateManager	= ContentUpdateManager.getInstance(getApplicationContext(), null);
+		if (! updateManager.isRunning() ) {
+			updateManager.manageAndBroadcastUpdates(ContentUpdateManager.CommandType.INIT_HISTORY, false);
+		}
+	}
+
+
+	public void loadView(int month, int day) {
+		// Orange bar
+		TextView tv			= (TextView) findViewById(R.id.bar_category);
+		tv.setTypeface(categoryTypeface);
+		tv.setText(this.getString(R.string.heading_history) );
+
+		StringBuilder sb		= new StringBuilder();
+		sb.append(day);
+		sb.append("/");
+		sb.append(month);
+		sb.append(" ");
+		sb.append(this.getString(R.string.heading_history_extra) );
+
+		tv							= (TextView) findViewById(R.id.bar_date);
+		tv.setText(sb.toString() );
+	}
+
+
+	public void reloadRows() {
+		Calendar cal			= Calendar.getInstance();
+		CursorAdapter ca	= (CursorAdapter) listAdapter;
+		// Reload rows
+		Cursor newCursor	= historyDAO.select(cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE) );
+		ca.changeCursor(newCursor);
+
+		// Make sure the top bar data is up-to-date
+		loadView(month, day);
+	}
+
+
+	// INNER CLASSES
+	private class HistoryUpdateBroadcastReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			reloadRows();
+		}
+	}
+}
