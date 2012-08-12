1dd5aedfa642381c8835b9f450d51bccf8844d25
Chris <chris-kobrzak@users.noreply.github.com>
2012-08-12 13:48:44 +0000

    Usunięto nieużywane metody delete.

--- src/pl/lewica/lewicapl/android/database/ArticleDAO.java
+++ src/pl/lewica/lewicapl/android/database/ArticleDAO.java
@@ -428,15 +428,4 @@ public class ArticleDAO {
 		return map;
 	}
 
-
-	/**
-	 * Not in use yet.  Convert this method to public when the delete functionality is implemented.
-	 * @param articleID
-	 * @return
-	 */
-	@SuppressWarnings("unused")
-	private boolean delete(long articleID) {
-		return database.delete(DATABASE_TABLE, FIELD_ID + "=" + articleID, null) > 0;
-	}
-
 }
