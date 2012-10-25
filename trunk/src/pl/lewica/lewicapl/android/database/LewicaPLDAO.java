c8005a9cd7545447f6d7380f423ea86f700f59e8
kobrzak@lewica.pl <kobrzak@lewica.pl@1cfff53c-f52c-0e3a-7529-82c1170abfbd>
2012-10-25 13:06:52 +0000

    Zmieniono sygnaturę klasy na abstrakcyjną.

--- trunk/src/pl/lewica/lewicapl/android/database/LewicaPLDAO.java
+++ trunk/src/pl/lewica/lewicapl/android/database/LewicaPLDAO.java
@@ -5,7 +5,7 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 
-public class LewicaPLDAO {
+public abstract class LewicaPLDAO {
 
 	private Context context;
 	protected SQLiteDatabase database;
