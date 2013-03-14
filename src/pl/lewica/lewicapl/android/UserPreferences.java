9e6295ddadd3d7525a2593fb118738e9e848f63f
Chris <chris-kobrzak@users.noreply.github.com>
2013-03-14 14:06:16 +0000

    Suwak zmiany rozmiaru czcionki jest teraz w pełni funkcjonalny na planszy artykułów

--- src/pl/lewica/lewicapl/android/UserPreferences.java
+++ src/pl/lewica/lewicapl/android/UserPreferences.java
@@ -7,12 +7,13 @@ import android.content.SharedPreferences.Editor;
 
 public class UserPreferences {
 
-	public static final float DEFAULT_FONT_SIZE_HEADING	= 24.f;
-	public static final float DEFAULT_FONT_SIZE_STANDARD	= 15.f;
-	public static final float MIN_FONT_SIZE_HEADING			= 12.f;
-	public static final float MIN_FONT_SIZE_STANDARD		= 10.f;
-	public static final float MAX_FONT_SIZE_HEADING			= 44.f;
-	public static final float MAX_FONT_SIZE_STANDARD		= 32.f;
+	public static final float DEFAULT_TEXT_SIZE_HEADING		= 24.f;
+	public static final float DEFAULT_TEXT_SIZE_STANDARD	= 15.f;
+	public static final float MIN_TEXT_SIZE_HEADING			= 16.f;
+	public static final float MIN_TEXT_SIZE_STANDARD		=  7.f;
+	public static final float MAX_TEXT_SIZE_HEADING			= 36.f;
+	public static final float MAX_TEXT_SIZE_STANDARD		= 27.f;
+	public static final float FONT_TEXT_INCREMENT			=  4.f;
 
 	public enum TextSizeAction	{
 		INCREASE,
@@ -21,10 +22,10 @@ public class UserPreferences {
 
 	public static void changeUserTextSize(TextSizeAction action, Activity activity) {
 		float textSize;
-		float change	= 4.f;
+		float change	= FONT_TEXT_INCREMENT;
 
 		if (action == TextSizeAction.DECREASE) {
-			change	= -4.f;
+			change	= -FONT_TEXT_INCREMENT;
 		}
 		textSize		= getUserTextSizeHeading(activity);
 		textSize	+= change;
@@ -39,11 +40,11 @@ public class UserPreferences {
 	public static float getUserTextSizeStandard(Activity activity) {
 		SharedPreferences prefs	= activity.getPreferences(Context.MODE_PRIVATE);
 
-		return prefs.getFloat("textSizeStandard", DEFAULT_FONT_SIZE_STANDARD);
+		return prefs.getFloat("textSizeStandard", DEFAULT_TEXT_SIZE_STANDARD);
 	}
 
 
-	private static void setUserTextSizeStandard(float size, Activity activity) {
+	public static void setUserTextSizeStandard(float size, Activity activity) {
 		SharedPreferences prefs	= activity.getPreferences(Context.MODE_PRIVATE);
 		Editor prefsEditor			= prefs.edit();
 		prefsEditor.putFloat("textSizeStandard", size);
@@ -54,12 +55,12 @@ public class UserPreferences {
 	public static float getUserTextSizeHeading(Activity activity) {
 		SharedPreferences prefs	= activity.getPreferences(Context.MODE_PRIVATE);
 		
-		return prefs.getFloat("textSizeHeading", DEFAULT_FONT_SIZE_HEADING);
+		return prefs.getFloat("textSizeHeading", DEFAULT_TEXT_SIZE_HEADING);
 	}
 
 
-	private static void setUserTextSizeHeading(float size, Activity activity) {
-		if (size < MIN_FONT_SIZE_HEADING || size > MAX_FONT_SIZE_HEADING) {
+	public static void setUserTextSizeHeading(float size, Activity activity) {
+		if (size < MIN_TEXT_SIZE_HEADING || size > MAX_TEXT_SIZE_HEADING) {
 			return;
 		}
 		SharedPreferences prefs	= activity.getPreferences(Context.MODE_PRIVATE);
@@ -71,12 +72,12 @@ public class UserPreferences {
 	
 	public static boolean canIncreaseTextSize(Activity activity) {
 		float size	= getUserTextSizeStandard(activity);
-		return size <= MAX_FONT_SIZE_STANDARD;
+		return size <= MAX_TEXT_SIZE_STANDARD;
 	}
 
 
 	public static boolean canDecreaseTextSize(Activity activity) {
 		float size	= getUserTextSizeStandard(activity);
-		return size >= MIN_FONT_SIZE_STANDARD;
+		return size >= MIN_TEXT_SIZE_STANDARD;
 	}
 }
