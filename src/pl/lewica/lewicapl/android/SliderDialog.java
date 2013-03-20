40fb65c8f354f9cab016a551b05816b733cd0f30
Chris <chris-kobrzak@users.noreply.github.com>
2013-03-20 16:30:53 +0000

    Porządkowanie kodu obsługującego okienko dialogowe z suwakiem (do zmiany rozmiaru czcionki)

---
+++ src/pl/lewica/lewicapl/android/SliderDialog.java
@@ -0,0 +1,49 @@
+package pl.lewica.lewicapl.android;
+
+public class SliderDialog {
+
+	private int mSliderMax; 
+	private int mSliderValue; 
+	private int mTitleResource;
+	private int mOkButtonResource;
+
+
+	public int getSliderMax() {
+		return mSliderMax;
+	}
+	
+	
+	public void setSliderMax(int sliderMax) {
+		this.mSliderMax = sliderMax;
+	}
+
+
+	public int getSliderValue() {
+		return mSliderValue;
+	}
+
+
+	public void setSliderValue(int sliderValue) {
+		this.mSliderValue = sliderValue;
+	}
+
+
+	public int getTitleResource() {
+		return mTitleResource;
+	}
+
+
+	public void setTitleResource(int titleResource) {
+		this.mTitleResource = titleResource;
+	}
+
+
+	public int getOkButtonResource() {
+		return mOkButtonResource;
+	}
+
+
+	public void setOkButtonResource(int mOkButtonResource) {
+		this.mOkButtonResource = mOkButtonResource;
+	}
+}
