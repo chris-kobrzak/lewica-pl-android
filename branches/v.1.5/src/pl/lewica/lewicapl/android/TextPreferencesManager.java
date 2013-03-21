66da2dc6e17bb04aae11ec11629c76d4478bd446
kobrzak@lewica.pl <kobrzak@lewica.pl@1cfff53c-f52c-0e3a-7529-82c1170abfbd>
2013-03-21 12:20:02 +0000

    Nie ma powodu, żeby klasa obsługująca ustawienia użytkownika wymagała obiektów Activity; zmieniono je na obiekty klasy Context.

--- branches/v.1.5/src/pl/lewica/lewicapl/android/TextPreferencesManager.java
+++ branches/v.1.5/src/pl/lewica/lewicapl/android/TextPreferencesManager.java
@@ -1,6 +1,6 @@
 package pl.lewica.lewicapl.android;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
@@ -30,25 +30,25 @@ public class TextPreferencesManager {
 	}
 
 
-	public static void switchTheme(ThemeHandler themeHandler, Activity activity) {
-		switch (getUserTheme(activity) ) {
+	public static void switchTheme(ThemeHandler themeHandler, Context context) {
+		switch (getUserTheme(context) ) {
 			case THEME_BLACK_ON_WHITE:
 				themeHandler.setThemeDark();
 
-				setUserTheme(THEME_WHITE_ON_BLACK, activity);
+				setUserTheme(THEME_WHITE_ON_BLACK, context);
 				break;
 
 			case THEME_WHITE_ON_BLACK:
 				themeHandler.setThemeLight();
 
-				setUserTheme(THEME_BLACK_ON_WHITE, activity);
+				setUserTheme(THEME_BLACK_ON_WHITE, context);
 				break;
 		}
 	}
 
 
-	public static void loadTheme(ThemeHandler themeHandler, Activity activity) {
-		switch (getUserTheme(activity) ) {
+	public static void loadTheme(ThemeHandler themeHandler, Context context) {
+		switch (getUserTheme(context) ) {
 			case THEME_BLACK_ON_WHITE:
 				themeHandler.setThemeLight();
 				break;
@@ -60,15 +60,15 @@ public class TextPreferencesManager {
 	}
 
 
-	private static int getUserTheme(Activity activity) {
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+	public static int getUserTheme(Context context) {
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 
 		return prefs.getInt(USER_SETTING_THEME, THEME_BLACK_ON_WHITE);
 	}
 
 
-	private static void setUserTheme(int theme, Activity activity) {
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+	private static void setUserTheme(int theme, Context context) {
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 		Editor prefsEditor			= prefs.edit();
 		prefsEditor.putInt(USER_SETTING_THEME, theme);
 		prefsEditor.commit();
@@ -91,33 +91,33 @@ public class TextPreferencesManager {
 	}
 
 
-	public static float getUserTextSize(Activity activity) {
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+	public static float getUserTextSize(Context context) {
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 
 		return prefs.getFloat(USER_SETTING_TEXT_SIZE, DEFAULT_TEXT_SIZE);
 	}
 
 
-	public static void setUserTextSize(float size, Activity activity) {
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+	public static void setUserTextSize(float size, Context context) {
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 		Editor prefsEditor			= prefs.edit();
 		prefsEditor.putFloat(USER_SETTING_TEXT_SIZE, size);
 		prefsEditor.commit();
 	}
 
 
-	public static float getUserTextSizeHeading(Activity activity) {
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+	public static float getUserTextSizeHeading(Context context) {
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 		
 		return prefs.getFloat(USER_SETTING_TEXT_SIZE_HEADING, DEFAULT_TEXT_SIZE_HEADING);
 	}
 
 
-	public static void setUserTextSizeHeading(float size, Activity activity) {
+	public static void setUserTextSizeHeading(float size, Context context) {
 		if (size < MIN_TEXT_SIZE_HEADING || size > MAX_TEXT_SIZE_HEADING) {
 			return;
 		}
-		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(activity);
+		SharedPreferences prefs	= PreferenceManager.getDefaultSharedPreferences(context);
 		Editor prefsEditor			= prefs.edit();
 		prefsEditor.putFloat(USER_SETTING_TEXT_SIZE_HEADING, size);
 		prefsEditor.commit();
