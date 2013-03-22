a7d8959d9cf5adbdcd52e7a9180408b8c4794f7a
kobrzak@lewica.pl <kobrzak@lewica.pl@1cfff53c-f52c-0e3a-7529-82c1170abfbd>
2013-03-22 16:29:52 +0000

    Jaskrawy tekst na listach artykułów został przyciemniony, kiedy ciemne tło jest w użyciu.  Zmiana ma na celu uniknięcie zbyt dużego kontrastu między tekstem a tłem.

--- branches/v.1.5/src/pl/lewica/lewicapl/android/activity/PublicationListActivity.java
+++ branches/v.1.5/src/pl/lewica/lewicapl/android/activity/PublicationListActivity.java
@@ -257,7 +257,7 @@ public class PublicationListActivity extends Activity {
 			}
 
 			if (TextPreferencesManager.getUserTheme(context) == TextPreferencesManager.THEME_WHITE_ON_BLACK) {
-				tvDate.setTextColor(res.getColor(R.color.white) );
+				tvDate.setTextColor(res.getColor(R.color.grey) );
 				view.setBackgroundColor(res.getColor(R.color.black) );
 			} else {
 				tvDate.setTextColor(res.getColor(R.color.grey_darker) );
