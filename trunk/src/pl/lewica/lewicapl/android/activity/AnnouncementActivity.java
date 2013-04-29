5ab520c42de5509da92fa987a87c2f537000a63b
kobrzak@lewica.pl <kobrzak@lewica.pl@1cfff53c-f52c-0e3a-7529-82c1170abfbd>
2013-04-29 21:00:37 +0000

    Kod wyświetlający widget do zmiany rozmiaru czcionki, który był identyczny w trzech różnych klasach został wydzielony do oddzielnej metody w nowej klasie użytkowej

--- trunk/src/pl/lewica/lewicapl/android/activity/AnnouncementActivity.java
+++ trunk/src/pl/lewica/lewicapl/android/activity/AnnouncementActivity.java
@@ -37,11 +37,10 @@ import pl.lewica.lewicapl.R;
 import pl.lewica.lewicapl.android.AndroidUtil;
 import pl.lewica.lewicapl.android.ApplicationRootActivity;
 import pl.lewica.lewicapl.android.BroadcastSender;
-import pl.lewica.lewicapl.android.DialogManager;
-import pl.lewica.lewicapl.android.SliderDialog;
 import pl.lewica.lewicapl.android.UserPreferencesManager;
 import pl.lewica.lewicapl.android.activity.util.StandardTextScreen;
 import pl.lewica.lewicapl.android.activity.util.TextSizeChangeListener;
+import pl.lewica.lewicapl.android.activity.util.TextSizeDialog;
 import pl.lewica.lewicapl.android.database.AnnouncementDAO;
 import pl.lewica.lewicapl.android.database.BaseTextDAO;
 import pl.lewica.lewicapl.android.theme.Theme;
@@ -174,14 +173,7 @@ public class AnnouncementActivity extends Activity implements StandardTextScreen
 				return true;
 
 			case R.id.menu_change_text_size:
-				int sizeInPoints	= UserPreferencesManager.convertTextSize(UserPreferencesManager.getTextSize(this) );
-				SliderDialog sd		= new SliderDialog();
-				sd.setSliderValue(sizeInPoints);
-				sd.setSliderMax(UserPreferencesManager.TEXT_SIZES_TOTAL);
-				sd.setTitleResource(R.string.heading_change_text_size);
-				sd.setOkButtonResource(R.string.ok);
-
-				DialogManager.showDialogWithSlider(sd, this, mSeekBarChangeListener);
+				TextSizeDialog.showDefaultTextSizeWidget(mSeekBarChangeListener, this);
 
 				return true;
 
