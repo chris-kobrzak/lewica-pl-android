2d1ae5954d9c9fabc2430b6f6ecf60138c43a27b
Chris Kobrzak <chris.kobrzak@gmail.com>
2026-08-20 13:22:18 +0200

    Bump version to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "pl.lewica.lewicapl"
     minSdk = 26
     targetSdk = 36
-    versionCode = 9
-    versionName = "2.0"
+    versionCode = 10
+    versionName = "2.1"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
